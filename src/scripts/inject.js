e6833e54265c88ff453a5cd2ea22c8a9435db90f
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-06 21:21:16 -0700

    WIP commit

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,10 +1,14 @@
 (function () {
+  const storage = chrome.storage.local;
   const OriginalNotification = Notification;
   const OriginalAudio = Audio;
 
   function CustomNotification(title, options) {
+    options.silent = true;
     const notification = new OriginalNotification(title, options);
     changeAudioConstructor();
+    const audio = new Audio;
+    audio.play();
     setTimeout(() => {
       window.Audio = OriginalAudio;
     }, 2000);
@@ -15,7 +19,12 @@
     function CustomAudio(src) {
       const audio = new OriginalAudio(src);
       // set to a temporary sound for now, can be updated later to use user-specified sound
-      audio.src = "https://cdn.freesound.org/previews/352/352653_4019029-lq.mp3";
+      chrome.storage.local.get(["uploadedAudio"], function (result) {
+        if (result.uploadedAudio) {
+          audio.src = result.uploadedAudio;
+        }
+      });
+      // audio.src = "https://cdn.freesound.org/previews/352/352653_4019029-lq.mp3";
       return audio;
     }
     CustomAudio.prototype = OriginalAudio.prototype;

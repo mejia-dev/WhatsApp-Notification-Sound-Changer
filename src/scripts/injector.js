1ae43f50e1c6713146b0486d1f8f21bca49de647
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-08 23:00:02 -0700

    Update injector to dispatch custom event with audio data

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -3,5 +3,13 @@
   const script = document.createElement("script");
   script.setAttribute("src", injectScript);
   script.setAttribute("type", "text/javascript");
+  script.onload = function () {
+    this.remove();
+    chrome.storage.local.get(["uploadedAudio"], function (result) {
+      if (result.uploadedAudio) {
+        document.dispatchEvent(new CustomEvent('WACustomNotificationSound', { detail: result.uploadedAudio }));
+      }
+    });
+  }
   document.head.appendChild(script);
 })();

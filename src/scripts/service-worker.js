b65e760e86788d60d9e8823273ba8d71bdf7b15b
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-10 23:58:48 -0700

    Check for offscreen documents and close before creating a new one

--- src/scripts/service-worker.js
+++ src/scripts/service-worker.js
@@ -1,26 +1,26 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message) => {
   if (message.title === "WAW-Alert-Triggered") {
-    console.log("Bees");
+    if (await chrome.offscreen.hasDocument()) {
+      chrome.offscreen.closeDocument();
+    }
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       if (tabs.length > 0) {
         let currentTab = tabs[0];
-        // Mute the current tab
-        chrome.tabs.update(currentTab.id, { muted: true }, function () {
-          console.log('Tab muted due to notification');
-        });
+        chrome.tabs.update(currentTab.id, { muted: true });
       }
     });
 
-    chrome.offscreen.createDocument({
+    await chrome.offscreen.createDocument({
       url: "audio_player/audio_player.html",
-      reasons: ["AUDIO_PLAYBACK","LOCAL_STORAGE"],
+      reasons: ["AUDIO_PLAYBACK"],
       justification: "Required for playing back audio while tab is muted"
     });
 
-    // chrome.runtime.sendMessage({
-    //   title: "WAW-Play-Audio",
-    //   audio: message.audio
-    // });
+    chrome.runtime.sendMessage({
+      title: "WAW-Play-Sound",
+      audio: message.audio
+    });
   }
 
 });

59495a83dacc7b966d997c2fb5556fb9ed146df0
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-11 00:12:06 -0700

    Update message.title

--- src/audio_player/audio-player.js
+++ src/audio_player/audio-player.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.runtime.onMessage.addListener((message) => {
-    if (message.title === "WAW-Play-Sound") {
+    if (message.title === "WANSC-Play-Sound") {
       const audio = new Audio;
       audio.src = message.audio;
       audio.play();

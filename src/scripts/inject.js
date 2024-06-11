ad044e8a004104f559f98d6afdd7f589c680255b
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-11 00:05:01 -0700

    Refactor injected script to only dispatch event on Notification

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,27 +1,11 @@
-document.addEventListener('WACustomNotificationSound', (e) => {
+document.addEventListener("WANSC-Initialize", () => {
   const OriginalNotification = Notification;
-  const OriginalAudio = Audio;
 
   function CustomNotification(title, options) {
     options.silent = true;
     const notification = new OriginalNotification(title, options);
-    changeAudioConstructor();
-    const audio = new Audio;
-    audio.play();
-    setTimeout(() => {
-      window.Audio = OriginalAudio;
-    }, 2000);
+    document.dispatchEvent(new CustomEvent("WANSC-Notification-Triggered"));
     return notification;
-  }
-
-  function changeAudioConstructor() {
-    function CustomAudio(src) {
-      const audio = new OriginalAudio(src);
-      audio.src = e.detail;
-      return audio;
-    }
-    CustomAudio.prototype = OriginalAudio.prototype;
-    window.Audio = CustomAudio;
   }
 
   CustomNotification.prototype = OriginalNotification.prototype;

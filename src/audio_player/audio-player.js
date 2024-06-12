566b9a11fc176e6f5bd045a96a3c14220c6c57a3
mejia-dev <aaron.mejia8484@gmail.com>
2024-06-11 22:33:26 -0700

    Make audio element send message when finished

--- src/audio_player/audio-player.js
+++ src/audio_player/audio-player.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (message.title === "WANSC-Play-Sound") {
       const audio = new Audio;
       audio.src = message.audio;
+      audio.addEventListener("ended", () => {
+        chrome.runtime.sendMessage({ title: "WANSC-Audio-Ended" });
+      })
       audio.play();
     }
   });
